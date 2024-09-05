c2f429bb89548ab21a8218d40487ff85b589a98b
Kaique Barbosa <kaiquebarbosa2103@gmail.com>
2024-09-04 21:21:13 -0300

    Alterações iniciais

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,15 @@
+
 import Image from "next/image";
-
-
+import { ChakraBaseProvider } from "@chakra-ui/react";
+// import { Link } from "@chakra-ui/next-js";
+import { Box } from '@chakra-ui/react'
+ 
 export default function Home() {
   return (
-    <main >
-      
-    </main>
+    <ChakraBaseProvider>
+      <Box border='10px' borderColor='gray.200' >
+        <p>Teste</p>
+      </Box>
+    </ChakraBaseProvider>
   );
 }
