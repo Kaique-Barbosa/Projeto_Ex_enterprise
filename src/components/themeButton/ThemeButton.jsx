b989e566e801d91f6f0fb76057acd0d6cbf376a2
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-10-06 16:41:09 -0300

    Adicionado persitência de tema na página

--- src/components/themeButton/ThemeButton.jsx
+++ src/components/themeButton/ThemeButton.jsx
@@ -2,24 +2,37 @@
 
 import MoonIcon from "@/icons/MoonIcon/MoonIcon";
 import SunIcon from "@/icons/SunIcon/SunIcon";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const ThemeButton = () => {
   const [isDark, setIsDark] = useState(true);
 
-  function toggleTheme() {
-    if (document.documentElement.classList.contains("dark")) {
+  useEffect(() => {
+    const theme = localStorage.getItem("theme");
+    if (theme == "dark") {
+      document.documentElement.classList.add("dark");
+      setIsDark(true);
+    } else {
       document.documentElement.classList.remove("dark");
+      setIsDark(false);
+    }
+  }, []);
+
+  function toggleTheme() {
+    if (html.classList.contains("dark")) {
+      document.documentElement.classList.remove("dark");
+      localStorage.setItem("theme", "light");
       setIsDark(false);
     } else {
       document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
       setIsDark(true);
     }
   }
 
   return (
     <button
-      className="size-12 p-2 rounded-lg hover:bg-[#e3e4e63d]"
+      className="size-10 p-2 rounded-lg hover:bg-[#e3e4e63d]"
       onClick={toggleTheme}
     >
       {isDark ? <SunIcon /> : <MoonIcon />}
