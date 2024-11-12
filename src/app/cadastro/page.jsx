137da554e2572c3498916971b9b48cd04285ca93
Israel Soares Porto <isaelporto15@gmail.com>
2024-11-12 14:49:24 +0000

    Ajustando posicionamento do furmulário de cadastro

--- src/app/cadastro/page.jsx
+++ src/app/cadastro/page.jsx
@@ -10,115 +10,116 @@ import logo from "@/public/img/logo.png";
 
 function page() {
   return (
-    <div className="min-h-svh h-full flex flex-col justify-center p-4 gap-8">
-      <header className="flex gap-2 justify-center items-center">
+    <div className="min-h-svh h-full flex flex-col justify-center items-center">
+      <header className="flex gap-2 justify-center items-center p-4">
         <Image className="size-16 object-cover" alt="Logo" src={logo} />
         <span className="inline text-2xl font-semibold">Ex Enterprise</span>
       </header>
 
-      <main className="flex items-center justify-center flex-1">
-        <div className="max-w-md w-full flex flex-col gap-6 bg-cinza-50/10 dark:bg-preto-850/10 p-4">
+      <main className="w-full max-w-screen-lg flex items-center justify-center flex-1 px-4 sm:px-8 lg:px-12 py-4">
+        <div className="w-full flex flex-col gap-6 p-4">
           <form className="flex flex-col" onSubmit={(e) => e.preventDefault()}>
-            <fieldset className="flex flex-col gap-4">
+            <fieldset className="flex flex-col gap-8">
               <legend className="text-center text-4xl mb-6">
                 Crie a sua conta
               </legend>
-              <div className="flex-1">
-                <label htmlFor="nome" className="label">
-                  Nome
-                </label>
-                <Input
-                  css="w-full"
-                  placeholder="Primeiro nome"
-                  type="text"
-                  id="nome"
-                  required
-                  tabIndex={1}
-                />
-              </div>
-              <div className="flex-1">
-                <label htmlFor="sobrenome" className="label">
-                  Sobrenome
-                </label>
-                <Input
-                  css="w-full"
-                  placeholder="Sobrenome completo"
-                  type="text"
-                  id="sobrenome"
-                  required
-                  tabIndex={2}
-                />
-              </div>
-              <div className="flex-1">
-                <label htmlFor="email" className="label">
-                  Email
-                </label>
-                <Input
-                  css="w-full"
-                  placeholder="@exemplo.com"
-                  type="email"
-                  id="email"
-                  required
-                  tabIndex={3}
-                />
-              </div>
-              <div className="flex-1">
-                <label htmlFor="telefone" className="label">
-                  Telefone
-                </label>
-                <InputTel
-                  css="w-full"
-                  placeholder="(99) 99999-9999"
-                  id="telefone"
-                  required
-                  tabIndex={4}
-                />
+              <div className="flex flex-wrap gap-x-8 gap-y-2">
+                <div className="flex-grow basis-80">
+                  <label htmlFor="nome" className="label">
+                    Nome
+                  </label>
+                  <Input
+                    css="w-full"
+                    placeholder="Primeiro nome"
+                    type="text"
+                    id="nome"
+                    required
+                    tabIndex={1}
+                  />
+                </div>
+                <div className="flex-grow basis-80">
+                  <label htmlFor="sobrenome" className="label">
+                    Sobrenome
+                  </label>
+                  <Input
+                    css="w-full"
+                    placeholder="Sobrenome completo"
+                    type="text"
+                    id="sobrenome"
+                    required
+                    tabIndex={2}
+                  />
+                </div>
+                <div className="flex-grow basis-80">
+                  <label htmlFor="email" className="label">
+                    Email
+                  </label>
+                  <Input
+                    css="w-full"
+                    placeholder="exemplo@mail.com"
+                    type="email"
+                    id="email"
+                    required
+                    tabIndex={3}
+                  />
+                </div>
+                <div className="flex-grow basis-80">
+                  <label htmlFor="telefone" className="label">
+                    Telefone
+                  </label>
+                  <InputTel
+                    css="w-full"
+                    placeholder="(99) 99999-9999"
+                    id="telefone"
+                    required
+                    tabIndex={4}
+                  />
+                </div>
+                <div className="flex-grow basis-80">
+                  <label htmlFor="senha" className="label">
+                    Senha
+                  </label>
+                  <InputPassword
+                    id="senha"
+                    placeholder="Defina sua senha"
+                    required={true}
+                    css="w-full"
+                    tabIndex={5}
+                  />
+                </div>
+                <div className="flex-grow basis-80">
+                  <label htmlFor="confirmar-senha" className="label">
+                    Confirmar senha
+                  </label>
+                  <InputPassword
+                    id="confirmar-senha"
+                    placeholder="Digite sua senha definida"
+                    required={true}
+                    css="w-full"
+                    tabIndex={6}
+                  />
+                </div>
               </div>
 
-              <div className="flex-1">
-                <label htmlFor="senha" className="label">
-                  Senha
-                </label>
-                <InputPassword
-                  id="senha"
-                  placeholder="Defina sua senha"
-                  required={true}
-                  css="w-full"
-                  tabIndex={5}
-                />
+              <div className="flex flex-wrap gap-x-8 gap-y-4 items-center">
+                <button
+                  type="submit"
+                  className="btn btn-accent text-white flex-grow basis-80 py-3"
+                  tabIndex={7}
+                >
+                  Criar conta
+                </button>
+                <Link
+                  href={"/login"}
+                  alt="Já tem uma conta? Faça seu login"
+                  className="text-center text-laranja-light dark:text-laranja-dark hover:underline hover:underline-offset-2 flex-grow basis-80"
+                  tabIndex={8}
+                >
+                  Já têm uma conta? Faça seu login
+                </Link>
               </div>
-
-              <div className="flex-1">
-                <label htmlFor="confirmar-senha" className="label">
-                  Confirmar senha
-                </label>
-                <InputPassword
-                  id="confirmar-senha"
-                  placeholder="Digite sua senha definida"
-                  required={true}
-                  css="w-full"
-                  tabIndex={6}
-                />
-              </div>
-
-              <button
-                type="submit"
-                className="btn btn-accent text-white w-full py-3"
-                tabIndex={7}
-              >
-                Criar conta
-              </button>
             </fieldset>
           </form>
-
-          <Link
-            href={"/login"}
-            alt="Já tem uma conta? Faça seu login"
-            className="text-center text-laranja-light dark:text-laranja-dark hover:underline hover:underline-offset-2"
-            tabIndex={8}
-          >
-            Já têm uma conta? Faça seu login
-          </Link>
         </div>
       </main>
     </div>
