97848625b4a1ec8e591bb25d1e6ccb4b29d6f92e
Israel Soares Porto <isaelporto15@gmail.com>
2024-11-26 15:00:43 +0000

    adicionando página de acesso bloqueado com mensagem de permissão negada

---
+++ src/app/acesso-bloqueado/page.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import logo from "@/public/img/logo.png";
+import { useRouter } from "next/navigation";
+
+export default function page() {
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <header className="flex gap-2 justify-center items-center p-4">
+        <Link alt="ir para home" href="/" className="flex items-center gap-2">
+          <Image className="size-16 object-cover" alt="Logo" src={logo} />
+          <span className="text-2xl font-semibold">Ex Enterprise</span>
+        </Link>
+      </header>
+
+      <main className="flex-1 flex flex-col items-center justify-center gap-6">
+        <span className="iconify mdi--block-helper size-40 text-accent"></span>
+
+        <div className="flex flex-col items-center gap-4">
+          <h1 className="text-center text-accent">Acesso bloqueado</h1>
+          <p className="text-center text-lg">
+            Você não tem permissão para acessar essa página.
+          </p>
+          <button
+            className="btn font-medium py-2 px-8 bg-transparent text-secondary rounded-full border-accent hover:bg-accent"
+            onClick={() => router.back()}
+          >
+            Voltar
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
