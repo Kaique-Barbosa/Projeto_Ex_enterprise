f4108f4821403fa4b99a8ef163ff9cbaa6e6cda8
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-11-29 09:41:39 -0300

    refatorando componente DropdownUser para simplificar a lógica de renderização

--- src/components/Dropdown/User/index.jsx
+++ src/components/Dropdown/User/index.jsx
@@ -5,65 +5,66 @@ import { useAuthContext } from "@/context/AuthContext";
 export default function DropdownUser() {
   const { isAuthenticated, user, logout } = useAuthContext();
 
-  if (isAuthenticated) {
-    return (
-      <div className="dropdown dropdown-bottom dropdown-end">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost font-normal text-base"
-        >
-          Ola, {user.nome} {user.sobrenome}
-        </div>
-        <ul className="dropdown-content menu absolute w-60 bg-primary rounded-lg shadow-lg mt-2">
-          <li className="dropdown-item">
-            <div className="flex flex-col items-start gap-2">
-              <p className="font-semibold text-base">Email</p>
-              <p>{user.email}</p>
-            </div>
-          </li>
-          <li className="dropdown-item">
-            <button
-              onClick={logout}
-              className="hover:bg-accent hover:text-white"
-            >
-              Sair
-            </button>
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return (
-      <div className="dropdown dropdown-bottom dropdown-end">
-        <div
-          tabIndex={0}
-          role="button"
-          className="btn btn-ghost font-normal text-base"
-        >
-          Voçê não está logado
-        </div>
-        <ul className="dropdown-content menu absolute w-60 bg-primary rounded-lg shadow-lg mt-2">
-          <li className="dropdown-item">
-            <Link
-              alt="fazer login"
-              href="/login"
-              className="hover:bg-accent hover:text-white"
-            >
-              Login
-            </Link>
-          </li>
-          <li className="dropdown-item">
-            <Link
-              alt="criar conta"
-              href="/cadastro"
-              className="hover:bg-accent hover:text-white"
-            >
-              Criar conta
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div className="dropdown dropdown-bottom dropdown-end">
+      {isAuthenticated ? (
+        <>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost font-normal text-base"
+          >
+            Ola, {user.nome} {user.sobrenome}
+          </div>
+          <ul className="dropdown-content menu absolute w-60 bg-primary rounded-lg shadow-lg mt-2">
+            <li className="dropdown-item">
+              <div className="flex flex-col items-start gap-2">
+                <p className="font-semibold text-base">Email</p>
+                <p>{user.email}</p>
+              </div>
+            </li>
+            <li className="dropdown-item">
+              <button
+                onClick={logout}
+                className="hover:bg-accent hover:text-white"
+              >
+                Sair
+              </button>
+            </li>
+          </ul>
+        </>
+      ) : (
+        <>
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost font-normal text-base"
+          >
+            Voçê não está logado
+          </div>
+
+          <ul className="dropdown-content menu absolute w-60 bg-primary rounded-lg shadow-lg mt-2">
+            <li className="dropdown-item">
+              <Link
+                alt="fazer login"
+                href="/login"
+                className="hover:bg-accent hover:text-white"
+              >
+                Login
+              </Link>
+            </li>
+            <li className="dropdown-item">
+              <Link
+                alt="criar conta"
+                href="/cadastro"
+                className="hover:bg-accent hover:text-white"
+              >
+                Criar conta
+              </Link>
+            </li>
+          </ul>
+        </>
+      )}
+    </div>
+  );
 }
