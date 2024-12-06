6a4faab2d2f58dc2ad4a1f85fe6f06a104ae5573
Israel Soares Porto <isaelporto15@gmail.com>
2024-12-06 18:19:41 +0000

    atualizando página de acesso bloqueado e adicionando middleware para evitar acesso direto

---
+++ src/app/imoveis/[id]/gerar-contrato/page.jsx
@@ -0,0 +1,12 @@
+import Footer from '@/components/Card/Footer'
+import Header from '@/components/Card/Header'
+import React from 'react'
+
+export default function GerarContrato() {
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+        <Header />
+        <Footer />
+    </div>
+  )
+}
--- src/middleware.js
+++ src/middleware.js
@@ -3,31 +3,37 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const path = request.nextUrl.pathname;
   const token = request.cookies.get("token");
+  const referrer = request.headers.get("referer");
 
+  console.log("referrer", referrer);
   console.log("path", path);
-  
+  console.log("token", token);
+
   // Páginas públicas (sem autenticação)
-  const publicRoutes = [
-    "/",
-    "/consultoria",
-    "/imoveis",
-    "/contato",
-    "/ebooks",
-  ];
+  const publicRoutes = ["/", "/consultoria", "/imoveis", "/contato", "/ebooks"];
 
   // Middleware para redirecionar o usuário para a home caso ele esteja logado
   if (token && ["/login", "/cadastro"].includes(path)) {
     return NextResponse.redirect(new URL("/", request.url));
   }
-  
+
   // Middleware para rotas públicas
   if (publicRoutes.includes(path)) {
     return NextResponse.next();
+  }
+
+  if (!token || !referrer.includes("/imoveis/:id") || !referrer) {
+    return NextResponse.redirect(new URL("/acesso-bloqueado", request.url));
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico)*)", "/login", "/cadastro"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico)*)",
+    "/login",
+    "/cadastro",
+    "/imoveis/:id/gerar-contrato",
+  ],
 };
