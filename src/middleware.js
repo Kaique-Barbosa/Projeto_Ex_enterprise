a5ce2ae5449667ea1517a7972b06051f3a90afec
Israel-SoaresPorto <isaelporto15@gmail.com>
2024-12-09 16:00:00 -0300

    alteração em middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,9 +3,10 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const path = request.nextUrl.pathname;
   const token = request.cookies.get("token");
-
+  
   // Páginas públicas (sem autenticação)
   const publicRoutes = ["/", "/consultoria", "/imoveis", "/contato", "/ebooks"];
+  const privateRoutes = ["/imoveis/gerar-contrato"];
 
   // Middleware para redirecionar o usuário para a home caso ele esteja logado
   if (token && ["/login", "/cadastro"].includes(path)) {
@@ -18,7 +19,7 @@ export function middleware(request) {
   }
 
   // Middleware para rotas privadas
-  if (path === "/imoveis/:id/gerar-contrato") {
+  if (privateRoutes.includes(path)) {
     if (!token) {
       return NextResponse.redirect(new URL("/acesso-bloqueado", request.url));
     }
@@ -32,6 +33,6 @@ export const config = {
     "/((?!api|_next/static|_next/image|favicon.ico)*)",
     "/login",
     "/cadastro",
-    "/imoveis/:id/gerar-contrato",
+    "/imoveis/gerar-contrato",
   ],
 };
